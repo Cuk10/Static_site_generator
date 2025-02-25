a8dc6fda4e90d8c86a648e103feb9f90ab626da5
Cuk10 <oskar.cuk@gmail.com>
2025-02-25 22:56:50 +0100

    LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,9 +1,10 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
+    
     def test_props_to_html(self):
         node1 = HTMLNode("p", "text", [], {"href": "https://www.google.com", "target": "_blank", })
         node1.props_to_html()
@@ -12,8 +13,21 @@ class TestHTMLNode(unittest.TestCase):
     def test2(self):
         node1 = HTMLNode("p", "text", [], {"href": "https://www.google.com", "target": "_blank", })
         print(node1)
-
     
+        
+    def test_leaf(self):
+        node = LeafNode("p", "this is text")
+        print(node.to_html())
+
+    def test_leaf2(self):
+        node = LeafNode("p", "this is text", {"href": "https://www.google.com", })
+        print(node.to_html())
+
+
+    def test_leaf3(self):
+        node = LeafNode(None, "this is text", {"href": "https://www.google.com", })
+        print(node.to_html())
+
 
 
 
