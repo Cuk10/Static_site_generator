e35eb515c5ae19afe242c6e08378142bf7428786
Cuk10 <oskar.cuk@gmail.com>
2025-02-28 22:44:47 +0100

    copy paste

--- src/main.py
+++ src/main.py
@@ -1,9 +1,35 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
 
 def main():
-    text_node = TextNode("text", TextType.BOLD)
-    print(text_node)
+    from_static_to_public()
+
+
+
+def from_static_to_public():
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    os.mkdir("public")
+    copy_paste("static", "public")
+    
+
+def copy_paste(path, destination):
+    files = os.listdir(path)
+    for file in files:
+        new_file = os.path.join(path, file)
+        if os.path.isfile(new_file):
+            shutil.copy(new_file, destination)
+        else:
+            new_destination = os.path.join(destination, file)
+            os.mkdir(os.path.join(destination, file))
+            copy_paste(new_file, new_destination)
+
+
+
+
+
 
 
 main()
